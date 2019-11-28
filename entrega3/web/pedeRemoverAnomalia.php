2071a5fd0f3d31ad5cb67f14dac9f9903f5a423c
inesmargarida <inesmargarida1618@gmail.com>
2019-11-28 19:18:27 +0000

    socialao

--- entrega3/web/pedeRemoverAnomalia.php
+++ entrega3/web/pedeRemoverAnomalia.php
@@ -2,7 +2,7 @@
     <body>
  		<h3>Nome da Anomalia a remover:</h3>
 		<form action="removerAnomalia.php" method="post">
-		<p><input type="visible" id="id" value="<?=$_REQUEST['id']?>"/></p>
+		<p><input type="visible" name="id" value="<?=$_REQUEST['id']?>"/></p>
 	
         <p><input type="submit" value="Validar"/></p>
 		</form>
--- entrega3/web/pedeRemoverItem.php
+++ entrega3/web/pedeRemoverItem.php
@@ -2,7 +2,7 @@
     <body>
  		<h3>Nome do Item a remover:</h3>
 		<form action="removerItem.php" method="post">
-        <p><input type="visible" id="id" value="<?=$_REQUEST['id']?>"/></p>
+        <p><input type="visible" name="id" value="<?=$_REQUEST['id']?>"/></p>
 	
         <p><input type="submit" value="Validar"/></p>
 		</form>
