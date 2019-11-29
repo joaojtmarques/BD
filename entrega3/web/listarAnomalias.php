42de253e8fc3794bafe87160130274592df30711
inesmargarida <inesmargarida1618@gmail.com>
2019-11-29 13:39:07 +0000

    yey

--- entrega3/web/listarAnomalias.php
+++ entrega3/web/listarAnomalias.php
@@ -11,70 +11,73 @@
     $local1=$_REQUEST['local1'];
     $local2=$_REQUEST['local2'];
 
-
 	try
 	{
 		$host="db.ist.utl.pt";
 		$user="ist189473";
 		$password= "bd2019";
-		$dbname=$user;
-
-		$db= new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        $dbname=$user;
+        
+        $db= new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db->beginTransaction();
+        $sql= "SELECT latitude FROM local_publico where nome = :local1;";
+        $sql2= "SELECT latitude FROM local_publico where nome = :local2;";
+        $sql3= "SELECT longitude FROM local_publico where nome = :local1;";
+        $sql4= "SELECT longitude FROM local_publico where nome = :local2;";
 
-        
-        $latitude1 = "SELECT latitude FROM local_publico where nome = :local1;";
-        $longitude1= "SELECT longitude FROM local_publico where nome = :local1;";
-        $latitude2= "SELECT latitude FROM local_publico where nome = :local2;";
-        $longitude2= "SELECT longitude FROM local_publico where nome = :local2;";
-        
-
-        $result=$db->prepare($latitude1);
+		$result=$db->prepare($sql);
         $result-> execute(array($local1));
 
-        echo($result);
-        $result1=$db->prepare($longitude1);
-        $result1-> execute(array($local1));
-        $result2=$db->prepare($latitude2);
+        foreach($result as $row)
+		{
+            $latitude1 = $row['latitude'];
+        }
+
+        $result2=$db->prepare($sql2);
         $result2-> execute(array($local2));
-        $result3=$db->prepare($longitude2);
-        $result3-> execute(array($local2));
+
+        foreach($result2 as $row)
+		{
+            $latitude2 = $row['latitude'];
+        }
+
+        $result3=$db->prepare($sql3);
+        $result3-> execute(array($local1));
+
+        foreach($result3 as $row)
+		{
+            $longitude1 = $row['longitude'];
+        }
+
+        $result4=$db->prepare($sql4);
+        $result4-> execute(array($local2));
+
+        foreach($result4 as $row)
+		{
+            $longitude2 = $row['longitude'];
+        }
 
         $minLatitude = min($latitude1, $latitude2);
         $maxLatitude = max($latitude1, $latitude2);
         $minLongitude = min($longitude1, $longitude2);
         $maxLongitude = max($longitude1, $longitude2);
-        
 
-        $sql = "SELECT anomalia.id from (select * from local_publico where (latitude > :minLatitude and latitude < :maxLatitude and longitude > :minLongitude and longitude < :maxLongitude)) as T natural join item INNER JOIN incidencia on incidencia.item_id = item.id INNER JOIN anomalia on anomalia.id = incidencia.anomalia_id group by anomalia.id;";
-        
+        $sql5 = "SELECT anomalia.id from (select * from local_publico where (latitude > :minLatitude and latitude < :maxLatitude and longitude > :minLongitude and longitude < :maxLongitude)) as T natural join item INNER JOIN incidencia on incidencia.item_id = item.id INNER JOIN anomalia on anomalia.id = incidencia.anomalia_id group by anomalia.id;";
 
+        $result5=$db->prepare($sql5);
+        $result5-> execute(array($minLatitude, $maxLatitude, $minLongitude, $maxLongitude));
 
-        
-        $result4=$db->prepare($sql);
-        $result4-> execute(array($maxLatitude, $maxLatitude, $minLatitude, $minLongitude));
-        echo($result1);
-        echo($maxLongitude);
-        echo($minLongitude);
-        echo($maxLatitude);
-        //$result4-> execute(array($local1));
-    
-
-
-        $result-> execute();
         echo("<table>\n");
-		echo("<tr><td >ANOMALIAS</td></tr>\n");
-		foreach($result as $row)
+		echo("<tr><td >UTILIZADORES</td></tr>\n");
+		foreach($result5 as $row)
 		{
             echo("<tr><td>");
-			echo($row['anomalia.id']);
+			echo($row['id']);
             echo("</td></tr>\n");
         }
         echo("</table>\n");
-        $db->commit();
 		$db=null;
-	}
+    }
 	catch(PDOException $e){
 		echo("<p>ERROR: {$e->getMessage()}</p>");
 	}
@@ -82,3 +85,4 @@
 ?>
 
 </html>
+
